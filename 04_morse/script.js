2d165f3cddf1804ec440e66e789c6f59d849e38b
Chloé T. <chloe.thomas14@gmail.com>
2023-11-02 15:04:43 +0100

    commit etape 1

---
+++ 04_morse/script.js
@@ -0,0 +1,7 @@
+const getLatinCharacterList = () => {
+    const enterCharacter = prompt("Entrez une chaine de caractère");
+    let splitCharacter = enterCharacter.split("")
+    console.log(splitCharacter)
+}
+
+getLatinCharacterList ()
