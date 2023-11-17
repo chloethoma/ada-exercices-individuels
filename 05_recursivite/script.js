cee56c11b93ecc2d4bd05fe02da1afc76ed1699f
Chloé T. <chloe.thomas14@gmail.com>
2023-11-17 16:29:26 +0100

    add last script

--- 05_recursivite/script.js
+++ 05_recursivite/script.js
@@ -32,50 +32,6 @@ const fibonacci = (x) => {
     }
     return x = fibonacci(x-1) + fibonacci(x-2)
 }
-console.log("Fibonacci 10 =", fibonacci(10))
-
-
-// Code Philippe
-const fibo = (x) => {
-    fiboArray = []
-      if (x <= 0) {
-        fiboArray.push(x)
-        return 0;
-      } else if (x === 1) {
-        fiboArray.push(x)
-        return 1;
-      } else if(x >= 2) {
-        const result = fibo(x - 1) + fibo(x - 2);
-        fiboArray.push(result)
-        return result
-      }
-    }
-  let result = fibo(10)
-  console.log("result:", result);
-  console.log("fiboArray:", fiboArray);
-
-  // Code ChatGPT
-  function fibonacciArray(n, result = []) {
-    if (n === 0) {
-      result.push(0);
-    } else if (n === 1) {
-      result.push(0, 1);
-    } else {
-      result = fibonacciArray(n - 1, result);
-      result.push(result[result.length - 1] + result[result.length - 2]);
-    }
-  
-    return result;
-  }
-  
-  // Exemple d'utilisation :
-  const n = 8; // Remplacez 8 par le nombre d'éléments de la suite que vous souhaitez
-  const fibArray = fibonacciArray(n);
-  console.log(fibArray);
-//   Cette fonction utilise un tableau result pour stocker les valeurs de la suite de Fibonacci au fur et à mesure. Elle ajoute la valeur actuelle comme la somme des deux valeurs précédentes. Vous pouvez ajuster la valeur de n pour obtenir une suite de Fibonacci de la longueur souhaitée.
-  
-  
-  
-  
+console.log("Fibonacci 10 =", fibonacci(10))  
   
   
